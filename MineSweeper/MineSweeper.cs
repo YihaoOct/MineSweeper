36f33b73440a159cbdc4ccc1a67b87e7ca1914cc
Yihao <yihao.wang@octopus.com>
2022-07-29 18:03:26 +1200

    AsReadOnly

--- MineSweeper/MineSweeper.cs
+++ MineSweeper/MineSweeper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace MineSweeper
@@ -11,7 +12,7 @@ namespace MineSweeper
         {
             // Since functional programming is about immutable states,
             // arrays are not a good data structure to use with FP.
-            // This method converts the 2D array to a nested List
+            // This method converts the 2D array to a nested ReadOnlyCollection
             // for the functional part to use, and then converts the 
             // result back to 2D array so that we don't change the
             // public interface.
@@ -19,8 +20,8 @@ namespace MineSweeper
             var (rows, cols) = (board.GetLength(0), board.GetLength(1));
 
             var mines = Enumerable.Range(0, rows)
-                .Select(r => Enumerable.Range(0, cols).Select(c => board[r, c]).ToList())
-                .ToList();
+                .Select(r => Enumerable.Range(0, cols).Select(c => board[r, c]).ToList().AsReadOnly())
+                .ToList().AsReadOnly();
 
             var hints = CalculateHintsFunctional(mines);
 
@@ -36,44 +37,44 @@ namespace MineSweeper
             return result;
         }
 
-        private List<List<int?>> CalculateHintsFunctional(List<List<char>> mines) =>
-            CalculateHintsFunctional(new List<List<int?>>(), mines, 0);
+        private ReadOnlyCollection<ReadOnlyCollection<int?>> CalculateHintsFunctional(ReadOnlyCollection<ReadOnlyCollection<char>> mines) =>
+            CalculateHintsFunctional(new List<ReadOnlyCollection<int?>>().AsReadOnly(), mines, 0);
 
-        private List<List<int?>> CalculateHintsFunctional(List<List<int?>> hints, List<List<char>> mines, int position) => 
+        private ReadOnlyCollection<ReadOnlyCollection<int?>> CalculateHintsFunctional(ReadOnlyCollection<ReadOnlyCollection<int?>> hints, ReadOnlyCollection<ReadOnlyCollection<char>> mines, int position) => 
             position == mines.Count
                 ? hints
                 : CalculateHintsFunctional(CalculateNextRow(hints, mines), mines, position + 1);
 
-        private List<List<int?>> CalculateNextRow(List<List<int?>> hints, List<List<char>> mines) =>
+        private ReadOnlyCollection<ReadOnlyCollection<int?>> CalculateNextRow(ReadOnlyCollection<ReadOnlyCollection<int?>> hints, ReadOnlyCollection<ReadOnlyCollection<char>> mines) =>
             hints.Count == 0
-                ? hints.Append(CalculateRow(GetInitialHints(mines.First()), mines.First())).ToList()
+                ? hints.Append(CalculateRow(GetInitialHints(mines.First()), mines.First())).ToList().AsReadOnly()
                 : hints.Take(hints.Count - 1)
                     .Append(CalculateRow(hints.Last(), mines[hints.Count]))
                     .Append(CalculateRow(
                         CalculateRow(GetInitialHints(mines[hints.Count]), mines[hints.Count]), 
                         mines[hints.Count - 1]))
-                    .ToList();
+                    .ToList().AsReadOnly();
 
-        private List<int?> CalculateRow(List<int?> hintRow, List<char> mineRow) => 
+        private ReadOnlyCollection<int?> CalculateRow(ReadOnlyCollection<int?> hintRow, ReadOnlyCollection<char> mineRow) => 
             CalculateRow(hintRow, mineRow, 0);
         
-        private List<int?> CalculateRow(List<int?> hintRow, List<char> mineRow, int position) =>
+        private ReadOnlyCollection<int?> CalculateRow(ReadOnlyCollection<int?> hintRow, ReadOnlyCollection<char> mineRow, int position) =>
             position == hintRow.Count
                 ? hintRow 
                 : mineRow[position] == mineCharacter
                     ? CalculateRow(CalculateRowForMineAt(hintRow, position), mineRow, position + 1)
                     : CalculateRow(hintRow, mineRow, position + 1);
 
-        private List<int?> CalculateRowForMineAt(List<int?> row, int position) =>
+        private ReadOnlyCollection<int?> CalculateRowForMineAt(ReadOnlyCollection<int?> row, int position) =>
             position == row.Count
                 ? row 
                 : row.Select((count, index) => 
                     count == null || index < position - 1 || index > position + 1
                         ? count 
-                        : count + 1).ToList();
+                        : count + 1).ToList().AsReadOnly();
 
-        private List<int?> GetInitialHints(List<char> row) => 
+        private ReadOnlyCollection<int?> GetInitialHints(ReadOnlyCollection<char> row) => 
             row.Select<char, int?>(c => c == mineCharacter ? null : 0)
-            .ToList();
+            .ToList().AsReadOnly();
     }
 }
